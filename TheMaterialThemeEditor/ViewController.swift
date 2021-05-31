db5c176b576b3e8080818c8c145c485fdc9292ac
0429oonishi <oonishireon110@gmail.com>
2021-05-31 23:20:16 +0900

    リファクタリング

--- TheMaterialThemeEditor/ViewController.swift
+++ TheMaterialThemeEditor/ViewController.swift
@@ -52,67 +52,33 @@ final class CustomBigView: UIView {
 
 final class ViewController: UIViewController {
     
-    @IBOutlet private weak var view1: CustomBigView!
-    @IBOutlet private weak var view2: CustomBigView!
-    @IBOutlet private weak var view3: CustomBigView!
-    @IBOutlet private weak var redStackView: UIStackView!
-    @IBOutlet private weak var orangeStackView: UIStackView!
-    @IBOutlet private weak var purpleStackView: UIStackView!
-    @IBOutlet private weak var greenStackView: UIStackView!
-    @IBOutlet private weak var skyBlueStackView: UIStackView!
-    @IBOutlet private weak var pinkStackView: UIStackView!
-    @IBOutlet private weak var blueStackView: UIStackView!
-    @IBOutlet private weak var yellowGreenStackView: UIStackView!
-    @IBOutlet private weak var purpleBlueStackView: UIStackView!
-    @IBOutlet private weak var pink2StackView: UIStackView!
-    @IBOutlet private weak var pink3StackView: UIStackView!
+    @IBOutlet private weak var mainColorView: CustomBigView!
+    @IBOutlet private weak var subColorView: CustomBigView!
+    @IBOutlet private weak var accentColorView: CustomBigView!
+    @IBOutlet private weak var themeColorStackView: UIStackView!
     
     private var selectedView: UIView?
     private var oldSelectedBigView: UIView?
-    private var allViews = [CustomView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view1.delegate = self
-        view2.delegate = self
-        view3.delegate = self
+        mainColorView.delegate = self
+        subColorView.delegate = self
+        accentColorView.delegate = self
         
-        configure(redStackView)
-        configure(orangeStackView)
-        configure(purpleStackView)
-        configure(greenStackView)
-        configure(skyBlueStackView)
-        configure(pinkStackView)
-        configure(blueStackView)
-        configure(yellowGreenStackView)
-        configure(purpleBlueStackView)
-        configure(pink2StackView)
-        configure(pink3StackView)
+        themeColorStackViewAction { $0.delegate = self }
         
-        view1.backgroundColor = redStackView.arrangedSubviews[0].backgroundColor
-        view2.backgroundColor = orangeStackView.arrangedSubviews[0].backgroundColor
-        view3.backgroundColor = purpleStackView.arrangedSubviews[0].backgroundColor
-        
-        configureImageView(view: view1)
-        configureImageView(view: view2)
-        configureImageView(view: view3)
-        view1.imageView.isHidden = false
-        oldSelectedBigView = view1
-        find(selectedView: view1)
+        setupImageView(view: mainColorView)
+        setupImageView(view: subColorView)
+        setupImageView(view: accentColorView)
+        mainColorView.imageView.isHidden = false
+        oldSelectedBigView = mainColorView
+        find(selectedView: mainColorView)
         
     }
     
-    private func configure(_ stackView: UIStackView) {
-        stackView.arrangedSubviews
-            .map { $0 as! CustomView }
-            .forEach {
-                $0.delegate = self
-                allViews.append($0)
-            }
-    }
-    
-    private func configureImageView(view: CustomBigView) {
+    private func setupImageView(view: CustomBigView) {
         view.addSubview(view.imageView)
         [view.imageView.heightAnchor.constraint(equalToConstant: 50),
          view.imageView.widthAnchor.constraint(equalToConstant: 50),
@@ -123,7 +89,7 @@ final class ViewController: UIViewController {
     }
     
     private func find(selectedView: UIView) {
-        allViews.forEach { view in
+        themeColorStackViewAction { view in
             let sameColor = (view.backgroundColor == selectedView.backgroundColor)
             let sameAlpha = (view.alpha == selectedView.alpha)
             if sameColor && sameAlpha {
@@ -131,6 +97,16 @@ final class ViewController: UIViewController {
                 self.selectedView = view
             }
         }
+    }
+    
+    private func themeColorStackViewAction(handler: (CustomView) -> Void) {
+        themeColorStackView.arrangedSubviews
+            .map { $0 as! UIStackView }
+            .forEach { stackView in
+                stackView.arrangedSubviews
+                    .map { $0 as! CustomView }
+                    .forEach { handler($0) }
+            }
     }
     
 }
